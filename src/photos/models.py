097a0b3c9728d9953a8101ec77a605b9c6924abb
Jon Rutter <69657008+jonrutter@users.noreply.github.com>
2021-04-30 18:14:13 -0400

    Homepage
    Homepage integrated with Django

--- src/photos/models.py
+++ src/photos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+def build_photos_path(instance, filename):
+    return 'images/{0}/{1}'.format(instance.category, filename)
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to=build_photos_path)
+    CATEGORY_CHOICES = [
+        ('people', 'people'),
+        ('animals', 'animals'),
+        ('nature', 'nature & landscapes'),
+        ('weddings', 'weddings')
+    ]
+    category = models.CharField(max_length=8, choices=CATEGORY_CHOICES)
+    alt_text = models.CharField('Alternate text for visually impaired users', max_length=200)
+    caption = models.CharField('Image caption', max_length=200)
+    href = models.URLField('Link to original', max_length=200)
